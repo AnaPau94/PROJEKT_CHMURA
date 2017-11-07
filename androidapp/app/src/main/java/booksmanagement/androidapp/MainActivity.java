fac1f9e666c8dbd54c393d785019cc96ce50d0fe
Bartek <bartek0794@gmail.com>
2017-11-07 15:04:13 +0100

    stworzenie activity do dodawania ksiazek

--- androidapp/app/src/main/java/booksmanagement/androidapp/MainActivity.java
+++ androidapp/app/src/main/java/booksmanagement/androidapp/MainActivity.java
@@ -1,36 +1,19 @@
 package booksmanagement.androidapp;
 
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import booksmanagement.androidapp.model.Book;
-
-public class MainActivity extends AppCompatActivity {
-    private final static String addBookURL = "http://192.168.43.60:8080/api/book/create";
-    private RestTemplate restTemplate;
-
+public class MainActivity extends Activity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        //Dodanie książki
-        restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-
-        Book book = new Book();
-        book.setBookAuthor("Henryk Sienkiewicz");
-        book.setBookTitle("From Mobile");
-        book.setIsbn("1234567891234");
-        HttpEntity<Book> request = new HttpEntity<>(book);
-        restTemplate.postForObject(addBookURL, request, Book.class);
+    }
+    public void openAddBookActivity(View view) {
+        Intent myIntent = new Intent(MainActivity.this,
+                AddBookActivity.class);
+        startActivity(myIntent);
     }
 }
