d3cde9703eb77ff1023448be2e96b7385b416141
Yevhenii Protsenko <yevhenii.protsenko@hycom.pl>
2017-11-07 14:09:52 +0100

    add to user two list of book

--- webapp/src/main/java/pl/booksmanagement/model/User.java
+++ webapp/src/main/java/pl/booksmanagement/model/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
@@ -16,4 +17,21 @@ public class User {
     private String username;
     private String password;
     private String email;
+
+    @OneToMany
+    @JoinTable(
+            name="user_owned_books",
+            joinColumns = @JoinColumn( name="id"),
+            inverseJoinColumns = @JoinColumn( name="book_id")
+    )
+    private List<Book> ownedBooks ;
+
+
+    @OneToMany
+    @JoinTable(
+            name="user_books_to_buy",
+            joinColumns = @JoinColumn( name="id"),
+            inverseJoinColumns = @JoinColumn( name="book_id")
+    )
+    private List<Book>  booksToBuy;
 }
