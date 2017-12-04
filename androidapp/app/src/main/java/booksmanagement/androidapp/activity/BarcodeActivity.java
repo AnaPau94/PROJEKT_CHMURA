f54d0747d4a970878a519d1e673241b0d1548540
Bartek <bartek0794@gmail.com>
2017-12-04 21:26:18 +0100

    pytanie o zezwolenie na użycie aparatu przy skanowaniu kodów kreskowych

--- androidapp/app/src/main/java/booksmanagement/androidapp/activity/BarcodeActivity.java
+++ androidapp/app/src/main/java/booksmanagement/androidapp/activity/BarcodeActivity.java
@@ -1,7 +1,11 @@
 package booksmanagement.androidapp.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +19,9 @@ import booksmanagement.androidapp.R;
 
 public class BarcodeActivity extends AppCompatActivity {
     private EditText resultISBN;
-    private Button searchBook;
+    private Button searchBook, scanBarcode;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+
     IntentResult scanningResult;
 
     @Override
@@ -24,6 +30,8 @@ public class BarcodeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_by_barcode);
         resultISBN = (EditText) findViewById(R.id.resultISBN);
         searchBook  = (Button) findViewById(R.id.scan_by_isbn);
+        scanBarcode  = (Button) findViewById(R.id.button_find_by_isbn);
+        checkCameraPermission();
     }
 
     public void scanBarcode(View view) {
@@ -41,6 +49,26 @@ public class BarcodeActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
+        }
+    }
+
+    public void checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_CAMERA_REQUEST_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    scanBarcode.setEnabled(true);
+                } else {
+                    scanBarcode.setEnabled(false);
+                }
+                return;
+            }
         }
     }
 }
