aaf4a5d398d2cacc8b5b1790ef5c5abbded65022
AVladyslav <187185@edu.p.lodz.pl>
2017-12-11 21:59:47 +0100

    Poprawnie działający szkic wyboru sklepu. Przejście do sklepu.

--- androidapp/app/src/main/java/booksmanagement/androidapp/activity/ShopSelectActivity.java
+++ androidapp/app/src/main/java/booksmanagement/androidapp/activity/ShopSelectActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import booksmanagement.androidapp.R;
 import booksmanagement.androidapp.model.BookModel;
@@ -12,16 +13,10 @@ import booksmanagement.androidapp.model.BookModel;
 public class ShopSelectActivity extends AppCompatActivity {
 
     BookModel book;
-    String part1Empik = "http://www.empik.com/z-importu,20,s?q=";
-    String part2Empik = "&qtype=basicForm";
-    String part1Matras = "";
-    String part2Matras = "";
-    String part1Merlin = "";
-    String part2Merlin = "";
-    String part1SwiatKsiazki = "";
-    String part2SwiatKsiazki = "";
-    String part1Ceneo = "";
-    String part2Ceneo = "";
+    String part1Empik = "http://www.empik.com/ksiazki,31,s?q=";
+    String part1Merlin = "https://merlin.pl/catalog/?q=";
+    String part1SwiatKsiazki = "https://www.swiatksiazki.pl/catalogsearch/result/?q=";
+    String part1Ceneo = "https://www.ceneo.pl/Ksiazki;szukaj-"; //ISBN wyszuiwanie nie dziaa. Szukanie wedug tytułu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +26,31 @@ public class ShopSelectActivity extends AppCompatActivity {
         //TODO dokończyć na razie na sztywno wpisany model książki
         book = new BookModel();
         book.setIsbn("9788376745473");
+        book.setBookTitle("Doktorzy z piekła rodem");
     }
 
     public void onButtonEmpikClick(View view) {
-        String url = part1Empik + book.getIsbn() + part2Empik;
+        String url = part1Empik + book.getIsbn();
         openWebPage(url);
     }
 
     public void onButtonMatrasClick(View view) {
-        //TODO dokończyć
+        Toast.makeText(this, R.string.matras_error, Toast.LENGTH_LONG).show();
     }
 
     public void onButtonMerlinClick(View view) {
-        //TODO dokończyć
+        String url = part1Merlin + book.getIsbn();
+        openWebPage(url);
     }
 
     public void onButtonSwiatKsiazkiClick(View view) {
-        //TODO dokończyć
+        String url = part1SwiatKsiazki + book.getIsbn();
+        openWebPage(url);
     }
 
     public void onButtonCeneoClick(View view) {
-        //TODO dokończyć
+        String url = part1Ceneo + book.getBookTitle();
+        openWebPage(url);
     }
 
     private void openWebPage(String url) {
